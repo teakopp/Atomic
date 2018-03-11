34ebba1ac5ce4cf2f6b334333252294c0143800f
Travis <drstrangelove@traviss-mbp-2.gateway.sonic.net>
2018-03-11 10:34:31 -0700

    initial commit

---
+++ Server/app.js
@@ -0,0 +1,24 @@
+
+const http = require("http")
+const MongoClient = require('mongodb').MongoClient;
+const hostname = '127.0.0.1'
+const port = 3000
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text-plain');
+  res.end('Hello World\n');
+
+  MongoClient.connect('mongodb://localhost:27017/Atomic', function(err, db) {
+  // If a database called "myNewDatabase" exists, it is used, otherwise it gets created.
+
+  let collection = db.collection('characters');
+    // If a collection called "myNewCollection" exists, it is used, otherwise it gets created.
+  });
+
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
