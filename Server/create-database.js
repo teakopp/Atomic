b600e9e42c1697c6f3c51b08f379cb038769145b
Travis <teakopp@github.com>
2018-03-13 15:55:00 -0700

    updated databse schema

--- Server/create-database.js
+++ Server/create-database.js
@@ -2,11 +2,24 @@ const MongoClient = require('mongodb').MongoClient;
 let url = "mongodb://localhost:27017/atomic"
 database = "atomic"
 collection = "players"
-data = {name : "name", description : "description",
-    refresh : "refresh", highconcept : "high concept", trouble : "trouble",
-    aspects : ["1", "2", "3"], skills : {"Athletics" : 3, "Fight" : 4 },
-    extras : "???", stunts : "???", physicalstress : 0, mentalstress : 0,
-    consequences : []}
+data = {
+        name : "name",
+        charactersheet : {
+          description : "description",
+          refresh : "refresh",
+          highconcept : "high concept",
+          trouble : "trouble",
+          aspects : ["1", "2", "3"],
+          skills : {"Athletics" : 3, "Fight" : 4 },
+          extras : "???",
+          stunts : "???",
+          physicalstress : 0,
+          mentalstress : 0,
+          consequences : []
+        },
+        items : [],
+        notes : []
+      }
 
 MongoClient.connect(url, (err, db)=>{
   if (err) throw err;
